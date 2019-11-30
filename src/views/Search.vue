dc8fa5c3a7ccdf564dfc5e501352c792a8c57541
khang00 <dlqkhang2@gmail.com>
2019-11-30 19:24:08 +0700

    aaaaaaaa

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="car-display">
-    <h2>Caree</h2>
+    <h2 class="heading">Caree</h2>
     <div class="search">
       <search />
       <i class="fas fa-sliders-h slider"></i>
@@ -11,14 +11,21 @@
     </div>
 
     <h2>Feature</h2>
-    <div class="feature">
-      <div class="big-car">
-        <car class="car" v-bind:car="this.cars[0]" width="40" />
-      </div>
-      <div class="small-cars">
-        <car class="car" v-bind:car="this.cars[1]" width="" direction="horizontal" />
-        <car class="car" v-bind:car="this.cars[2]" direction="horizontal" />
-        <car class="car" v-bind:car="this.cars[3]" direction="horizontal" />
+    <div class="feature-wrapper">
+      <div class="feature">
+        <div class="big-car">
+          <car class="car" v-bind:car="this.cars[0]" width="40" />
+        </div>
+        <div class="small-cars">
+          <car
+            class="car"
+            v-bind:car="this.cars[1]"
+            width=""
+            direction="horizontal"
+          />
+          <car class="car" v-bind:car="this.cars[2]" direction="horizontal" />
+          <car class="car" v-bind:car="this.cars[3]" direction="horizontal" />
+        </div>
       </div>
     </div>
 
@@ -75,15 +82,19 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap");
 .car-display {
   position: relative;
-  width: 100%;
+  width: 80%;
   height: 100%;
   font-family: "Montserrat";
   display: flex;
   flex-direction: column;
-  align-items: center;
-  
+  padding: 0 10%;
+
   h2 {
     font-size: 40px;
+  }
+
+  .heading{
+    text-align: center;
   }
 
   .search {
@@ -95,46 +106,53 @@ export default {
     .slider {
       transform: scale(1.5, 1.5);
       transition: 300ms;
-      margin-left: 3%;
+      margin-left: 5%;
+
       &:hover {
         cursor: pointer;
       }
     }
-
-    width: 100%;
-    display: flex;
   }
 
   .car-filter {
     position: relative;
-    margin: 2.3%;
-    display: inline-block;
+    width: 100%;
+    margin-top: 40px;
+    display: flex;
+    justify-content: center;
 
     .filter {
+      width: 70%;
       position: relative;
-      padding: 4% 0%;
+      padding: 3% 0%;
     }
   }
 
-  .feature {
+  .feature-wrapper {
     display: flex;
-    width: 80%;
-    align-items: center;
-    justify-content: space-between;
+    width: 100%;
+    justify-content: center;
 
-    .big-car{
-      margin-right: 3%;
-    }
-
-    .small-cars{
-      height: 100%;
+    .feature {
       display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-    }
-
-    .small-cars .car{
       width: 100%;
+      align-items: center;
+      justify-content: space-between;
+
+      .big-car {
+        margin-right: 3%;
+      }
+
+      .small-cars {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+
+      .small-cars .car {
+        width: 100%;
+      }
     }
   }
 
